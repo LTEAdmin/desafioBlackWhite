3aa2a1ae310afd17b62a3d87e6168804d77bbbcc
Alejandra Flaño <aflanog@latiaencanto.cl>
2024-05-03 20:54:58 -0400

    se vincula index.html con style.css

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+//rutas estaticas a traves de middlewares
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/views', express.static(__dirname + '/views'));
+
+//ruta home 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.sendFile('/views/index.html', { root: __dirname });
 });
 
+//ruta generica
 app.get('*', (req, res) => {
     res.status(404).send('Pagina no encontrada, por favor revise la URL');
 });
